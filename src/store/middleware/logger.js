512c16be54c08bce291f309dadc3a95ed336a22d
Blake Ferm <blakeferm@gmail.com>
2023-04-13 07:18:50 -0400

    updating logger code

---
+++ src/store/middleware/logger.js
@@ -0,0 +1,14 @@
+// currying function - custom logger
+export const loggerMiddleWare = (store) => (next) => (action) => {
+    if (!action.type) {
+        return next(action);
+    }
+
+    console.log('type: ', action.type);
+    console.log('payload: ', action.payload);
+    console.log('currentState: ', store.getState());
+
+    next(action);
+
+    console.log('next state: ', store.getState());
+};
